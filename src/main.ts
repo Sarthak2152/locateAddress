1004fa7df490db9607d26ebb4178a2494f1670ea
Sarthak <sarthakapoor2152@gmail.com>
2024-04-29 02:25:16 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import mapboxgl from "mapbox-gl";
+
+const form = document.querySelector("form")!;
+const userInput = <HTMLInputElement>document.getElementById("address")!;
+
+const KEY: string = import.meta.env.VITE_MAPBOX_KEY;
+
+const handleSubmit = async (event: Event) => {
+  event.preventDefault();
+  try {
+    const address = userInput.value;
+    const response = await axios.get(
+      `https://api.mapbox.com/search/geocode/v6/forward?`,
+      {
+        params: {
+          q: encodeURI(address),
+          access_token: KEY,
+        },
+      }
+    );
+
+    if (response.data.features.length > 0) {
+      const [lng, lat] = response.data.features[0].geometry.coordinates;
+      mapboxgl.accessToken = KEY;
+      const map = new mapboxgl.Map({
+        zoom: 10,
+        container: "map", // container ID
+        center: [lng, lat],
+      });
+    }
+  } catch (error) {
+    console.log("An error occurred");
+  } finally {
+    userInput.value = "";
+  }
+};
+form.addEventListener("submit", handleSubmit);
